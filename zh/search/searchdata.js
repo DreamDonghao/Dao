41f8a1b2c2302f4fa7ade90b429750ae966098d4
DreamDonghao <DreamDonghao@users.noreply.github.com>
2025-12-07 07:25:37 +0000

    deploy: 3749cd2e7b3496fb7d018d6a94927fb712afffbb

---
+++ zh/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdegimnopqrstuvw~",
+  1: "abgmptw",
+  2: "de",
+  3: "abegmpqtw",
+  4: "abcgimnoprstuw~",
+  5: "aimnrv",
+  6: "t",
+  7: "e"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "enums",
+  7: "enumvalues"
+};
+
+var indexSectionLabels =
+{
+  0: "全部",
+  1: "类",
+  2: "命名空间",
+  3: "文件",
+  4: "函数",
+  5: "变量",
+  6: "枚举",
+  7: "枚举值"
+};
+
